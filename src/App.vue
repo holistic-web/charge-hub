1ceaaf87c9d5e58cb4c9b3d678ba2d88c1bdcf75
Michael Fitzhavey <michael.fitzhavey@gmail.com>
2020-12-22 00:08:18 +0000

    feat: firebase implementation

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,46 @@
 <template>
     <section class="App">
         <h1 class="App__title">Charge Hub</h1>
+        {{ pins }}
         <google-map class="App__map" />
     </section>
 </template>
 
 <script>
+import firebase from 'firebase';
 import GoogleMap from './components/GoogleMap';
+
+firebase.initializeApp({
+	apiKey: 'AIzaSyCzrOC9Sa6UyZXbffHgjfU_5GKPTH-RHpg',
+	authDomain: 'charge-hub.firebaseapp.com',
+	databaseURL: 'https://charge-hub-default-rtdb.europe-west1.firebasedatabase.app',
+	projectId: 'charge-hub',
+	storageBucket: 'charge-hub.appspot.com',
+	messagingSenderId: '760484226654',
+	appId: '1:760484226654:web:a7685ea54c3d05a1c976cf',
+	measurementId: 'G-64CVS8G661'
+});
+
+const db = firebase.firestore();
 
 export default {
     components: {
         GoogleMap,
     },
+    data() {
+        return {
+            pins: []
+        }
+    },
+    methods: {
+        async loadPins() {
+            const querySnapshot = await db.collection('charge-locations').get();
+            querySnapshot.forEach(doc => this.pins.push(doc.data()));
+        }
+    },
+    created() {
+        this.loadPins();
+    }
 };
 </script>
 

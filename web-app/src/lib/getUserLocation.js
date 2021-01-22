3b517a48da8e6fe1ea6133c1b4f2cc67b0dbc2d5
Michael Fitzhavey <michael.fitzhaavey@gmail.com>
2021-01-22 00:47:47 +0000

    chore: better usage of location api

--- web-app/src/lib/getUserLocation.js
+++ web-app/src/lib/getUserLocation.js
@@ -1,6 +1,10 @@
 export default async () => {
     const pos = await new Promise((resolve, reject) => {
-        navigator.geolocation.getCurrentPosition(resolve, reject);
+        navigator.geolocation.getCurrentPosition(resolve, reject, {
+			enableHighAccuracy: true,
+			timeout: 5000,
+			maximumAge: 0
+		});
     });
 
     return {

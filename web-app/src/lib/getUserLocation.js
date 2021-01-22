62a3ae4f8467b5ffafe07a3fc44a9d14c16e1f3c
Lint Action <lint-action@samuelmeuli.com>
2021-01-22 02:11:37 +0000

    Fix code style issues with ESLint

--- web-app/src/lib/getUserLocation.js
+++ web-app/src/lib/getUserLocation.js
@@ -1,10 +1,10 @@
 export default async () => {
     const pos = await new Promise((resolve, reject) => {
         navigator.geolocation.getCurrentPosition(resolve, reject, {
-			enableHighAccuracy: true,
-			timeout: 5000,
-			maximumAge: 0
-		});
+            enableHighAccuracy: true,
+            timeout: 5000,
+            maximumAge: 0,
+        });
     });
 
     return {

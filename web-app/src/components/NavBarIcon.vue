59bee95d460243f47b52fc5b0794355f47bdfb8e
maxcramer <maximiliancramer@yahoo.com>
2021-02-05 22:33:41 +0000

    updating width of nav icons to 100% so spaced evenly. Created NavBarIcon component to render each icon from menuItems array in Navbar component

---
+++ web-app/src/components/NavBarIcon.vue
@@ -0,0 +1,45 @@
+<template>
+    <router-link class="NavBarIcon" :to="{ name: 'map' }">
+        <img class="NavBarIcon__image" :src="image"/>
+        <span v-text="text"/>
+    </router-link>
+</template>
+
+
+<script>
+export default {
+    props: {
+        to: {
+            type: Object,
+            required: true
+        },
+        image: {
+            type: String,
+            required: true
+        },
+        text: {
+            type: String,
+            required: true
+        },
+    }
+}  
+
+</script>
+
+<style lang="scss">
+.NavBarIcon {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
+    text-decoration: none;
+    width: 100%;
+
+    &.router-link-active {
+        font-weight: bold;
+    }
+    &__image {
+
+    }
+}
+</style>

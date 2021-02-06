b78de8f17d970e337eabc12009eb3e37c1377284
Michael Fitzhavey <michael.fitzhaavey@gmail.com>
2021-02-06 16:41:59 +0000

    chore: test web app deployment

--- web-app/src/lib/geocode.js
+++ web-app/src/lib/geocode.js
@@ -1,16 +1,16 @@
 // Thanks to  https://stackoverflow.com/a/46064393 for the head start here, it's now somewhat modified
 export default function geocode(options, geocoder) {
-    return new Promise(function(resolve, reject) {
-        geocoder.geocode(options, function(results, status) {
-            if (status === 'OK') {
-                resolve(results[0]);
-            } else {
-                reject(
-                    new Error(
-                        "Couldnt't find the location " + JSON.stringify(options)
-                    )
-                );
-            }
-        });
-    });
+	return new Promise(function (resolve, reject) {
+		geocoder.geocode(options, function (results, status) {
+			if (status === 'OK') {
+				resolve(results[0]);
+			} else {
+				reject(
+					new Error(
+						"Couldnt't find the location " + JSON.stringify(options)
+					)
+				);
+			}
+		});
+	});
 }
--- web-app/src/router/index.js
+++ web-app/src/router/index.js
@@ -8,42 +8,42 @@ import AddLocation from '../views/AddLocation';
 Vue.use(VueRouter);
 
 const routes = [
-    {
-        path: '/map',
-        name: 'map',
-        component: Map,
-    },
-    {
-        path: '/user',
-        name: 'user',
-        component: User,
-    },
-    {
-        path: '/login',
-        name: 'login',
-        component: Login,
-    },
-    {
-        path: '/add-location',
-        name: 'add-location',
-        component: AddLocation,
-    },
-    {
-        path: '*',
-        redirect: { name: 'map' },
-    },
+	{
+		path: '/map',
+		name: 'map',
+		component: Map,
+	},
+	{
+		path: '/user',
+		name: 'user',
+		component: User,
+	},
+	{
+		path: '/login',
+		name: 'login',
+		component: Login,
+	},
+	{
+		path: '/add-location',
+		name: 'add-location',
+		component: AddLocation,
+	},
+	{
+		path: '*',
+		redirect: { name: 'map' },
+	},
 ];
 
 const router = new VueRouter({
-    mode: 'history',
-    base: process.env.BASE_URL,
-    routes,
+	mode: 'history',
+	base: process.env.BASE_URL,
+	routes,
 });
 
 router.beforeEach((to, from, next) => {
-    const user = window.localStorage.getItem('user');
-    if (to.name !== 'login' && !user) next({ name: 'login' });
-    next();
+	const user = window.localStorage.getItem('user');
+	if (to.name !== 'login' && !user) next({ name: 'login' });
+	next();
 });
 
 export default router;

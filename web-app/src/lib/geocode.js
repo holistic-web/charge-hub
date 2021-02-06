036f3ac1b961cbe8817be18ca7fe6e848e5d8cf5
Lint Action <lint-action@samuelmeuli.com>
2021-02-06 22:55:07 +0000

    Fix code style issues with ESLint

--- web-app/src/lib/geocode.js
+++ web-app/src/lib/geocode.js
@@ -2,20 +2,20 @@ import loadGoogleMaps from './loadGoogleMaps';
 
 // Thanks to  https://stackoverflow.com/a/46064393 for the head start here, it's now somewhat modified
 export default async function geocode(options) {
-	const googleService = await loadGoogleMaps();
-	const geocoder = new googleService.maps.Geocoder();
+    const googleService = await loadGoogleMaps();
+    const geocoder = new googleService.maps.Geocoder();
 
-	return new Promise(function (resolve, reject) {
-		geocoder.geocode(options, function (results, status) {
-			if (status === 'OK') {
-				resolve(results[0]);
-			} else {
-				reject(
-					new Error(
-						"Couldnt't find the location " + JSON.stringify(options)
-					)
-				);
-			}
-		});
-	});
+    return new Promise(function(resolve, reject) {
+        geocoder.geocode(options, function(results, status) {
+            if (status === 'OK') {
+                resolve(results[0]);
+            } else {
+                reject(
+                    new Error(
+                        "Couldnt't find the location " + JSON.stringify(options)
+                    )
+                );
+            }
+        });
+    });
 }

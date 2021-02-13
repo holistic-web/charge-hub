a3cda7f062bc426a292ed588aa5c92ca512eb332
Lint Action <lint-action@samuelmeuli.com>
2021-02-13 01:58:01 +0000

    Fix code style issues with Prettier

--- cloud-functions/triggers/onChargeLocationWrite.js
+++ cloud-functions/triggers/onChargeLocationWrite.js
@@ -4,10 +4,10 @@ const admin = require('firebase-admin');
 admin.initializeApp();
 
 module.exports = (change, context) => {
-	const { before, after } = change;
-	const { timestamp, auth } = context;
-	functions.logger.log('before:', before);
-	functions.logger.log('after:', after);
-	functions.logger.log('timestamp:', timestamp);
-	functions.logger.log('auth:', auth);
+    const { before, after } = change;
+    const { timestamp, auth } = context;
+    functions.logger.log('before:', before);
+    functions.logger.log('after:', after);
+    functions.logger.log('timestamp:', timestamp);
+    functions.logger.log('auth:', auth);
 };

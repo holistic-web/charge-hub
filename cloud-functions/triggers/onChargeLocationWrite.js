23a252c79ca7d3d02d4a4c72168c02bedba0839e
Michael Fitzhavey <michael.fitzhaavey@gmail.com>
2021-02-13 01:45:42 +0000

    test: use firebase log function

--- cloud-functions/triggers/onChargeLocationWrite.js
+++ cloud-functions/triggers/onChargeLocationWrite.js
@@ -1,4 +1,9 @@
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+
+admin.initializeApp();
+
 module.exports = (change, context) => {
-    console.log('change: ', change);
-    console.log('context: ', context);
+	functions.logger.log('change', change);
+	functions.logger.log('context', context);
 };

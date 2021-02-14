5edddab9df6e5285d858b27f9ad480cef61fc995
Michael Fitzhavey <michael.fitzhaavey@gmail.com>
2021-02-14 11:07:33 +0000

    feat: simple geohashing cloud function

--- cloud-functions/triggers/onChargeLocationWrite.js
+++ cloud-functions/triggers/onChargeLocationWrite.js
@@ -1,13 +1,27 @@
-const functions = require('firebase-functions');
 const admin = require('firebase-admin');
+const _ = require('lodash');
+const geofire = require('geofire-common');
 
 admin.initializeApp();
 
-module.exports = (change, context) => {
-	const { before, after } = change;
-	const { timestamp, auth } = context;
-	functions.logger.log('before:', before);
-	functions.logger.log('after:', after);
-	functions.logger.log('timestamp:', timestamp);
-	functions.logger.log('auth.uid:', auth.uid);
+function hasLocationChanged(beforeData, afterData) {
+	if (!beforeData) return false;
+	if (!afterData) return false;
+	return _.isEqual(beforeData.location, afterData.location);
+}
+
+module.exports = (change) => {
+
+	const originalDocument = change.before.data();
+	const newDocument = change.after.data();
+
+	if (newDocument && (!originalDocument || hasLocationChanged(originalDocument, newDocument))) {
+		newDocument.location = {
+			...newDocument.location,
+			geoHash: geofire.geohashForLocation([newDocument.location.latitude, newDocument.location.longitude]),
+			geoHashedAt: admin.firestore.FieldValue.serverTimestamp()
+		};
+	}
+
+	return change.after.ref.set(newDocument);
 };
